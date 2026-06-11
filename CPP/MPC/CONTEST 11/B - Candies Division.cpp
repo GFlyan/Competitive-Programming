b5e960cc704bc71d858e9694602f4cfbc81f3a46
Flyan <guilhermeflyan.22@gmail.com>
2026-06-11 19:01:49 -0300

    CONTEST 11-B

---
+++ CPP/MPC/CONTEST 11/B - Candies Division.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        if (n%k > k/2) n -= n%k - k/2;
+        cout << n << "\n";
+    }
+    return 0;
+}
